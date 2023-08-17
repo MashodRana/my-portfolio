1fe12cf547136a9cd21d437aacb14634649afd9c
MashodRana <mashod0rana@gmail.com>
2023-08-17 15:57:19 +0600

    [DONE] side menu done

---
+++ src/components/GrayDivider.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GrayDivider = () => {
+  return (
+    <hr className="bg-gray-200 h-[1.5px]" />
+  )
+}
+
+export default GrayDivider
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
 import fbIcon from "../assets/icons/fbIcon.svg";
+import GrayDivider from "../components/GrayDivider";
+import { FaPhone, FaHome } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import skillIcon from "../assets/icons/skillIcon.svg";
+import rightArrowIcon from "../assets/icons/rightArrowIcon.svg";
+import downloadIcon from "../assets/icons/downloadIcon.svg";
+import heroImg from "../assets/heroImage.png"
 
 const Home = () => {
   return (
     <div className="bg-[#FBFBFC] flex justify-between ">
-      <div className="w-[250px] bg-white px-8 py-12 flex flex-col gap-6 justify-center items-ceter">
+      <div className="w-[300px] bg-white px-8 py-12 flex flex-col gap-6 justify-center items-ceter">
         <div className="flex flex-col gap-3.5 justify-center items-center">
           <div className="pb-4">
             <img
@@ -30,14 +37,96 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <hr className="bg-gray-200 h-[1.5px]" />
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
+        <GrayDivider />
+        {/* Contact Information */}
+        <div className="text-sm gray-text flex flex-col gap-2">
+          <div className="flex items-center gap-3">
+            <div className="pbgc text-white rounded-full w-8 h-8 flex justify-center items-center ">
+              <FaPhone className="w-8" />
+            </div>{" "}
+            <span>+880 1775 646995</span>
+          </div>
+          <div className="flex items-center gap-3">
+            <div className="pbgc text-white rounded-full w-8 h-8 flex justify-center items-center ">
+              <SiGmail className="w-8" />
+            </div>
+            <span>mashod0rana@gmail.com</span>
+          </div>
+          <div className="flex items-center gap-3">
+            <div className="pbgc text-white rounded-full w-8 h-8 flex justify-center items-center ">
+              {" "}
+              <FaHome className="w-8" />
+            </div>
+            <span>Azmpur Kacha Bazar, Dakshinkhan, Dhaka-1230, Bandladesh</span>
+          </div>
+        </div>
+
+        <GrayDivider />
+
+        <div className="flex flex-col gap-1.5">
+          <p className="flex gap-2">
+            <img className="w-5 h-5" src={skillIcon} alt="" /> Python, Django,
+            FastApi, DRF
+          </p>
+          <p className="flex gap-2">
+            <img className="w-5 h-5" src={skillIcon} alt="" /> JavaScript,
+            React.js, Node.js
+          </p>
+          <p className="flex gap-2">
+            <img className="w-5 h-5" src={skillIcon} alt="" /> Bootstrap,
+            Tailwindcss
+          </p>
+          <p className="flex gap-2">
+            <img className="w-5 h-5" src={skillIcon} alt="" /> Docker, Git,
+            Postman
+          </p>
+          <p className="flex gap-2">
+            <img className="w-5 h-5" src={skillIcon} alt="" /> Tensorflow,
+            Spacy, NLTK
+          </p>
+        </div>
+        <div>
+          <span className="text-sm gray-text">Bsc. in CSE</span>
+        </div>
+        <div className="flex flex-col gap-2">
+          <span className="text-sm gray-text">Bangla</span>
+          <span className="text-sm gray-text">English</span>
+          <span className="text-sm gray-text">Hindi</span>
+        </div>
+        <GrayDivider />
+        <div className="flex justify-center items-center">
+          <button className="py-2.5 px-10 pbgc flex gap-5 items-center text-white">
+            <span className="text-sm font-semibold">RESUME</span>
+            <img className="w-4 h-4" src={downloadIcon} alt="" />
+          </button>
+        </div>
       </div>
-      <div className="w-[750px]">
-        <h1 className="text-2xl">I’m Rayan Adlrdard Full-stack Developer </h1>
+      <div className="w-[970px]">
+        <section className="hero-section w-full flex ">
+          <div className="w-1/2 flex flex-col gap-6 ">
+            <h1 className="text-2xl pt-24 text-4xl font-bold">
+              I’m Mashod Rana
+              <br />
+              <span className="sc">Full-stack</span> Developer{" "}
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et,
+              volutpat feugiat placerat lobortis. Natoque rutrum semper sed
+              suspendisse nunc lectus.
+            </p>
+            <div>
+              <button className="flex gap-2 items-center font-medium text-base text-white pbgc px-8 py-4 rounded">
+                <span>HIRE ME</span>
+                <img src={rightArrowIcon} alt="" />
+              </button>
+            </div>
+          </div>
+          <div className="w-1/2 flex justify-center items-center">
+            <div className="px-12 py-7 bg-white rounded-lg">
+              <img className="w-[150px] h-[150px] rounded-full" src={heroImg} alt="" />
+            </div>
+          </div>
+        </section>
       </div>
       <div className="w-[110px]">nav</div>
     </div>
